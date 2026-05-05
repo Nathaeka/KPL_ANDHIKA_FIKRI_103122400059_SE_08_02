1157e3728a345f7ca61de959d9e42eabf9081f76
Andhika fikri 59 <fikridhika420@gmail.com>
2026-05-05 20:29:11 +0700

    Delete TP directory

--- TP/main.js
+++
@@ -1,9 +0,0 @@
-// Mengimpor pustaka menggunakan standar ESM
-import { hitungHuruf, hitungKata } from 'pustaka-hitung-teks';
-
-const teksUji = "Tes! hitung teks dari 123.";
-
-console.log("--= HASIL PENGUJIAN PUSTAKA =--");
-console.log("Teks Asli    :", teksUji);
-console.log("Jumlah Huruf :", hitungHuruf(teksUji));
-console.log("Jumlah Kata  :", hitungKata(teksUji));
